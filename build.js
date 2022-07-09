47f10ead96dc70f262c801ec6633e9ee66159a28
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-09 20:37:58 +0700

    Update

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+require("fs").rmSync("types", { recursive: true });
+
 require("child_process").exec("npx tsc");
 
 require("esbuild").buildSync({
@@ -8,5 +10,6 @@ require("esbuild").buildSync({
     bundle: true,
     outfile: "./index.js",
     platform: "node",
-    minify: true
+    minify: true,
+    legalComments: "none"
 });
