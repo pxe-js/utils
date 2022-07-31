4927ebd1d96ae3ccbdc9cb8bb7e9f6511f035f6d
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-31 20:05:46 +0700

    [1.0.0] Adding route utils to get exported routes from a folder

---
+++ src/routes.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import path from "path/posix";
+import type Router from "@pxe/router";
+import { Module } from "module";
+
+async function resolveImport(path: string) {
+    const res = await import(path);
+    return res.default ?? res;
+}
+
+/**
+ * @param dir 
+ */
+export function routesSync(dir: string): Router[] {
+    if (!module.require)
+        // @ts-ignore
+        module.require = Module.createRequire(import.meta?.url);
+
+    const list = [];
+
+    for (const file of fs.readdirSync(dir)) {
+        const filePath = path.resolve(dir, file);
+
+        if (filePath.endsWith("index.js"))
+            list.push(module.require(filePath));
+        else if (fs.statSync(filePath).isDirectory())
+            list.push(...routesSync(filePath));
+    }
+
+    return list;
+}
+
+export async function routes(dir: string): Promise<Router[]> {
+    const list = [];
+
+    for (const file of fs.readdirSync(dir)) {
+        const filePath = path.resolve(dir, file);
+
+        if (filePath.endsWith("index.js"))
+            list.push(await resolveImport(dir));
+        else if (fs.statSync(filePath).isDirectory())
+            list.push(...routesSync(filePath));
+    }
+
+    return list;
+}
+
