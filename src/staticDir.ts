8b1ffe48c0d64f73038a503aeb53a608156553a4
Revenity <87530663+aquapi@users.noreply.github.com>
2022-08-10 12:09:31 +0700

    [1.0.0] Set response body to a Buffer instead of converting it to a string

--- src/staticDir.ts
+++ src/staticDir.ts
@@ -1,12 +1,12 @@
 import { Middleware } from "@pxe/server";
-import fs from "fs";
+import fs from "fs/promises";
 
 function staticDirectory(dir: string = "public"): Middleware {
     return async (ctx, next, ...args) => {
         const path = ctx.request.url.substring(1);
 
         if (path.startsWith(dir))
-            ctx.response.body = fs.readFileSync("./" + path).toString();
+            ctx.response.body = await fs.readFile("./" + path);
 
         await next(...args);
     }
